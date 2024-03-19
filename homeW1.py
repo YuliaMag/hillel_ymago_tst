ad56278110fc406a1dc4193d006a7da6e11d0bcc
Yulia Magonya <ymagonia@gmail.com>
2024-03-20 01:32:15 +0200

    Add files via upload

---
+++ homeW1.py
@@ -0,0 +1,25 @@
+# Variables:
+
+var = 'test1'
+
+
+var1 = 1
+vAr1 = 1
+
+
+the_var = 2.1
+
+
+i1 = 10
+
+
+_test = '_test'
+
+
+djinn = input("Ask for a wish: ")
+print("You asked for {}. Do you really need it?".format(djinn))
+
+
+
+
+
